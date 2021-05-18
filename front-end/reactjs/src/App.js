c704698813dd9ec553cdba615bcdfc056676e8ae
stavroskostopoulos <77120544+stavroskostopoulos@users.noreply.github.com>
2021-05-18 23:15:03 +0300

    just a save

---
+++ front-end/reactjs/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+		
+		
+
+		<div class="SideNavBar" style={{width:"16%"}}>
+			
+			<h3>Kappito</h3>		
+
+			<div class="NavBar">
+				<a href="#" class="navChoice">Overview</a>
+				<a href="#" class="navChoice">Backlog</a>
+				<a href="#" class="navChoice">Projects</a>
+			</div>
+
+
+		</div>
+		<div class="TopBar"></div>
+		
+    </div>
+  );
+}
+
+export default App;
