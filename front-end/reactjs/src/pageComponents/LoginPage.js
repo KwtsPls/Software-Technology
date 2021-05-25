a7c73ad7d260d10922946419f43534eaac81a37a
stavroskostopoulos <77120544+stavroskostopoulos@users.noreply.github.com>
2021-05-25 21:10:28 +0300

    Update LoginPage.js

--- front-end/reactjs/src/pageComponents/LoginPage.js
+++ front-end/reactjs/src/pageComponents/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import logo from '../images/logo.png';
+import partners from '../images/partners.png';
 
 import '../App.css'
 import '../login.css'
