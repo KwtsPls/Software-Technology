e31d42182b43c30b2d89bcc74a28292d7e9f7690
Kimon Softas <kimon2199@hotmail.com>
2021-05-29 18:09:57 +0300

    Billing Popup done

--- front-end/reactjs/src/components/BankPopUp.js
+++ front-end/reactjs/src/components/BankPopUp.js
@@ -1,95 +1,74 @@
 import React, { Component } from 'react';
 import '../App.css';
-import backlogicon from '../images/backlogout.png'
-import backloghovericon from '../images/backloghover.png'
-import overviewicon from '../images/overviewout.png'
-import overviewhovericon from '../images/overviewhover.png'
-import projecticon from '../images/projectout.png'
-import projecthovericon from '../images/projecthover.png'
-import userphoto from '../images/userphoto.png'
-import settingsicon from '../images/settingsout.png'
-import settingshovericon from '../images/settingshover.png'
-import { Link } from 'react-router-dom'
-import { useEffect, useState } from "react";
 import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PaymentIcon from 'react-payment-icons-inline';
 
 
 
 
 function BankPopUp(props){
 
+    const months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    const years = [2021,2022,2023,2024,2025,2026]
+
     return (
         <div>
             <Modal
                 {...props}
-                size="lg"
+                size="md"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
                 >
-                <Modal.Header closeButton>
+                <Modal.Header>
                     <Modal.Title id="contained-modal-title-vcenter">
-                    Modal heading
+                    Payment Details
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                        </Form.Row>
-
-                        <Form.Group controlId="formGridAddress1">
-                            <Form.Label>Address</Form.Label>
-                            <Form.Control placeholder="1234 Main St" />
-                        </Form.Group>
-
-                        <Form.Group controlId="formGridAddress2">
-                            <Form.Label>Address 2</Form.Label>
-                            <Form.Control placeholder="Apartment, studio, or floor" />
-                        </Form.Group>
-
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridCity">
-                            <Form.Label>City</Form.Label>
-                            <Form.Control />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>State</Form.Label>
-                            <Form.Control as="select" defaultValue="Choose...">
-                                <option>Choose...</option>
-                                <option>...</option>
-                            </Form.Control>
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>Zip</Form.Label>
-                            <Form.Control />
-                            </Form.Group>
-                        </Form.Row>
-
-                        <Form.Group id="formGridCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                    <form class="row g-3">
+                        <div class="col-12">
+                            <label for="inputAddress" class="form-label">Cardholder Name</label>
+                            <input type="text" class="form-control" id="cardholderName" placeholder="John Doe"/>
+                        </div>
+                        <div class="col-12">
+                            <label for="inputAddress2" class="form-label">Credit Card Number</label>
+                            <input type="text" class="form-control" id="creditCardNumber" placeholder="0000 0000 0000 0000" minlength="16" maxlength="16"/>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="inputExpMonth" class="form-label">Expiry Month</label>
+                            <select id="inputExpMonth" class="form-select">
+                                <option selected>Choose...</option>
+                                {months.map(i => <option key={i}>{i}</option>)}
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="inputExpYear" class="form-label">Expiry Year</label>
+                            <select id="inputExpYear" class="form-select">
+                                <option selected>Choose...</option>
+                                {years.map(i => <option key={i}>{i}</option>)}
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="inputCvv" class="form-label">CVV</label>
+                            <input type="password" class="form-control" id="inputCvv" placeholder="000" minlength="3" maxlength="3"/>
+                        </div>
+                        <PaymentIcon icon="visa" style={{ margin: 10, width: 70 }} />
+                        <PaymentIcon icon="mastercard" style={{ margin: 10, width: 70 }} />
+                        <PaymentIcon icon="maestro" style={{ margin: 10, width: 70 }} />
+                        <PaymentIcon icon="paypal" style={{ margin: 10, width: 70 }} />
+                        <div class="col-8">
+                            {/* <Link to='/home'> */}
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            {/* </Link> */}
+                        </div>
+                    </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
+                    <Button variant="outline-danger" onClick={props.onHide}>Close</Button>
                 </Modal.Footer>
-                </Modal>
+            </Modal>
 		</div>
     );
 }
--- front-end/reactjs/src/pageComponents/settings/SettingsBillingPage.js
+++ front-end/reactjs/src/pageComponents/settings/SettingsBillingPage.js
@@ -1,64 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../../App.css';
-import SideNavBar from '../../components/SideNavBar.js'
-import { Link } from 'react-router-dom'
-import Topbar from '../../components/Topbar.js'
 import '../../css/settings.css';
+import SideNavBar from '../../components/SideNavBar.js'
+import Topbar from '../../components/Topbar.js'
+import BankPopUp from '../../components/BankPopUp.js'
 
 
 
-class SettingsBillingPage extends Component {
-    render() { 
-        return (
-            <div>
-                <Topbar/>
+function SettingsBillingPage() {
 
-                <SideNavBar/>
+    const [modalShow, setModalShow] = useState(false);
 
-                <div className="mainContent">
-                    
-                    
-                    
-
-                    <div className="jete-settingsMenu">
-                        <div class="list-group">
-                            <Link to='/settings/profile' class="list-group-item list-group-item-action">Profile</Link>
-                            <Link to='/settings/personalisation' class="list-group-item list-group-item-action">Personalisation</Link>
-					        <Link to='/settings/security' class="list-group-item list-group-item-action">Security</Link>
-                            <a href="#" class="list-group-item list-group-item-action  active">Billing</a>
-                        </div>
+    return (
+        <div>
+            <Topbar/>
+            <SideNavBar/>
+            <div className="mainContent">
+                <div className="jete-settingsMenu">
+                    <div class="list-group">
+                        <Link to='/settings/profile' class="list-group-item list-group-item-action">Profile</Link>
+                        <Link to='/settings/personalisation' class="list-group-item list-group-item-action">Personalisation</Link>
+					    <Link to='/settings/security' class="list-group-item list-group-item-action">Security</Link>
+                        <a href="#" class="list-group-item list-group-item-action  active">Billing</a>
                     </div>
+                </div>
+                <div className="settingsContains">
+                    <h1 className="settings-header">Account billing</h1>
+                    <hr className="new4"/>
 
-                    <div className="settingsContains">
-                        <h1 className="settings-header">Account billing</h1>
-                        <hr className="new4"/>
+                    <p className="settings-unit">
+                        <b>Current account status:</b> &emsp; &nbsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &ensp;Jete Premium
+                    </p>
 
-                        <p className="settings-unit">
-                            <b>Current account status:</b> &emsp; &nbsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &ensp;Jete Premium
-                        </p>
-
-                        <p className="expirationdate">Expires on: &emsp;04/04/21</p>
+                    <p className="expirationdate">Expires on: &emsp;04/04/21</p>
                         
-                        <Link to='/paymentPlan'>
+                    <Link to='/paymentPlan'>
                         <button type="button" className="btn btn-outline-danger paymentplan-button-change">Change payment plan</button>
-                        </Link>
+                    </Link>
 
-                        <p className="settings-unit">
-                            <b>Credit card:</b>&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &ensp;7XXX-XXXX-XXXX-X420
-                        </p>
-
-                        <button type="button" class="btn btn-outline-warning creditcard-button-change">Change credit card</button>
-                        
-
+                    <p className="settings-unit">
+                        <b>Credit card:</b>&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &ensp;7XXX-XXXX-XXXX-X420
+                    </p>
+                    <button type="button" onClick={() => setModalShow(true)} class="btn btn-outline-warning creditcard-button-change">Change credit card</button>
+                    <BankPopUp show={modalShow} onHide={() => setModalShow(false)}/>
                     </div>
                     
                         
                         
-                    </div>
-                
             </div>
-        );
-    }
+                
+        </div>
+    );
 }
  
 export default SettingsBillingPage;
