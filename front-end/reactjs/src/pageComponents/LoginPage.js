0c22acb1c9918108f580654bc5c8951821905844
Kimon Softas <kimon2199@hotmail.com>
2021-06-04 18:42:05 +0300

    workig on login

--- front-end/reactjs/src/pageComponents/LoginPage.js
+++ front-end/reactjs/src/pageComponents/LoginPage.js
@@ -7,7 +7,7 @@ import '../App.css'
 import '../css/login.css'
 
 
-class LoginPage extends Component {
+function LoginPage() {
 
     
     signUpStyle = {
@@ -15,61 +15,71 @@ class LoginPage extends Component {
 
     }
 
-    render() { 
-        return (
-            <div className="loginbackground">
-                <div className="login-screen">
-                    <div className="login-desc-box">
-                        <img className = "jete_logo-icon-login" src={logo} alt="logo"></img>        
-                        <p className="desc-header">Καλωσήρθατε</p>
-                        <img className = "partners" src={partners} alt="partnerssketch"></img>        
+    function sendLoginCredentials() {
+        // Simple PUT request with a JSON body using fetch
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: 'user', password: 'pass'})
+        };
+        fetch('http://localhost:8080/users/1', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // JSON data parsed by `data.json()` call
+              });
+            setTimeout(function(){window.location.reload();}, 100);
+    }
 
-                    </div>
-                    <div className="login-form-box">
-                        <div className="login-form">
-                                        
-                            <div className="col-auto">
-                                <div>
-                                    <div className="top-buffer">
-                                        <h1 className="text-center login-header">
-                                            Σύνδεση
-                                        </h1>
-                                        <form className="login-form-boxes">
-                                            <div className="form-group pt-3">
-                                                <label for="exampleInputEmail1">Email</label>
-                                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Πληκτρολογίστε το email σας"/>
-                                            </div>
-                                            <div className="form-group pt-3">
-                                                <label for="exampleInputPassword1">Κωδικός πρόσβασης</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Πληκτρολογίστε τον κωδικό σας"/>
-                                            </div>
-                                            <div className="form-check pt-3">
-                                                <input type="checkbox" className="form-check-input mycheckbox" id="exampleCheck1"/>
-                                                <label className="form-check-label" for="exampleCheck1">Remember me</label>
-                                            </div>
-                                            <div className="text-center pt-3">
-                                                <Link to='/home'>
-                                                    <button type="submit" className="btn-lg btn-primary login-button">Σύνδεση</button>
-                                                </Link>
-                                            </div>
-                                            <div className="text-center pt-3">
-                                                <small id="emailHelp" className="form-text text-muted">Είναι η πρώτη σας φορά εδώ; </small>
-                                                <Link to='/signUp'>
-                                                    <small id="signUpLink" className="form-text signup-link" style={this.signUpStyle}> Εγγραφείτε</small>
-                                                </Link>
-                                            </div>
-                                        </form>
-                                    </div>
+    return (
+        <div className="loginbackground">
+            <div className="login-screen">
+                <div className="login-desc-box">
+                    <img className = "jete_logo-icon-login" src={logo} alt="logo"></img>        
+                    <p className="desc-header">Καλωσήρθατε</p>
+                    <img className = "partners" src={partners} alt="partnerssketch"></img>        
+                </div>
+                <div className="login-form-box">
+                    <div className="login-form">
+                                    
+                        <div className="col-auto">
+                            <div>
+                                <div className="top-buffer">
+                                    <h1 className="text-center login-header">
+                                        Σύνδεση
+                                    </h1>
+                                    <form className="login-form-boxes">
+                                        <div className="form-group pt-3">
+                                            <label for="exampleInputEmail1">Email</label>
+                                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Πληκτρολογίστε το email σας"/>
+                                        </div>
+                                        <div className="form-group pt-3">
+                                            <label for="exampleInputPassword1">Κωδικός πρόσβασης</label>
+                                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Πληκτρολογίστε τον κωδικό σας"/>
+                                        </div>
+                                        <div className="form-check pt-3">
+                                            <input type="checkbox" className="form-check-input mycheckbox" id="exampleCheck1"/>
+                                            <label className="form-check-label" for="exampleCheck1">Remember me</label>
+                                        </div>
+                                        <div className="text-center pt-3">
+                                            <Link to='/home'>
+                                                <button type="submit" className="btn-lg btn-primary login-button">Σύνδεση</button>
+                                            </Link>
+                                        </div>
+                                        <div className="text-center pt-3">
+                                            <small id="emailHelp" className="form-text text-muted">Είναι η πρώτη σας φορά εδώ; </small>
+                                            <Link to='/signUp'>
+                                                <small id="signUpLink" className="form-text signup-link" style={this.signUpStyle}> Εγγραφείτε</small>
+                                            </Link>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
-            
-        );
-    }
+        </div>
+    );
 }
  
 export default LoginPage;
