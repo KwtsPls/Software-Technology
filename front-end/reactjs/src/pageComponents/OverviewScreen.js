4c4220a43d7beb2a1a6668ab92a2eefbdbfb4d59
stavroskostopoulos <77120544+stavroskostopoulos@users.noreply.github.com>
2021-07-01 00:28:36 +0300

    homepage halfway done

--- front-end/reactjs/src/pageComponents/OverviewScreen.js
+++ front-end/reactjs/src/pageComponents/OverviewScreen.js
@@ -1,41 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import '../css/Overviewscreen.css'
+import sunphoto from '../images/sun2.png'
 
 
 
-class OverviewScreen extends Component {
 
-    constructor(props) {
-        super(props);
-        this.loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
 
-    }
 
-    
-   
-    
-    componentDidMount() {
-        
+function OverviewScreen() {
+
+    const history = useHistory();
+    const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+    const [isLoading, setLoading] = useState(true);
+    const [tilecounter, setTileCounter] = useState(0);
+
+
+    useEffect(() => {
         document.body.style.background = "#fff";
 
+        if (!loggedUser){
+            history.push("/login");
+        }
+        else{
+            document.body.style.background = "#fff";
+
+            fetch('http://localhost:8080/users', {
+                method: 'get', 
+                headers: { Authorization: 'Bearer ' + loggedUser.accessToken }
+            })
+                .then(res => res.json())
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                })
+        }
+
+    }, []);
+
+
+    function get_recentProjectTile(name){
+        
+        // find a way to get only the first three projects
+        if(setTileCounter>3){
+            console.log(1);
+
+        }
+
+        return(
+            <a className="col-md-2 offset-md-1 mt-4 project-tile text-center">
+                {/* {name} */}
+                <h5 className="mt-3 mb-3 project-tile-name">Project 1</h5>
+            </a> 
+        );
+
     }
 
 
-    render() {
+    return (
+        <div className="homepage">
 
-        // this.displayaccordingtotime();
-        return (
-            <div>
+            <div className="row mt-5 d-flex justify-content-center">
+                <div className="col-md-6 pt-3 pb-3  greetingstile">
+                    <h1 className="homepage-text text-center greetings-text">Καλησπέρα, kostopez</h1>
+
+                    
+                </div>
+                <div className="col-md-2 pt-3 pb-3  text-center greetingstile tile-sun">
+                    <img className = "sun-img" src={sunphoto} alt="avatar"/>
+                </div>
+                
 
                 
-                τιποτα
 
                 
             
             </div>
             
+            <div className="recproj">
+                <div className="row mt-5 d-flex justify-content-center">
+                    <div className="col-md-8 mt-5 bg-trans ">
+                        <h2 className="homepage-text">Πρόσφατα projects &bull; </h2>
 
-        );
-    }
+                    </div>
+                </div>
+
+                <div className="row mt-4 offset-md-1 tilesrow">
+                    
+                     {/* for the first three projects of the user call this with i.title as argument */}
+                        {get_recentProjectTile(0)}
+                        {get_recentProjectTile(0)}
+                        {get_recentProjectTile(0)}
+
+                        {/* {projectList.map(i => i.title.toLowerCase().includes(searchVal.toLowerCase()) &&
+                    
+                        )} */}
+                    
+                </div>
+
+            </div>
+        
+        </div>
+
+    );
 }
+
  
 export default OverviewScreen;
