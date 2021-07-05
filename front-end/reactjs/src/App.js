4a3685fd4ea190615e319b807f3d018c182a293a
Kimon Softas <kimon2199@hotmail.com>
2021-07-05 21:56:20 +0300

    Removed personalisation from side bar + removed chat files

--- front-end/reactjs/src/App.js
+++ front-end/reactjs/src/App.js
@@ -9,12 +9,9 @@ import LoginPage from './pageComponents/LoginPage.js'
 import SignUpPage from './pageComponents/SignUpPage.js'
 import PaymentPlanPage from './pageComponents/PaymentPlanPage.js'
 import SettingsSecurityPage from './pageComponents/settings/SettingsSecurityPage.js'
-import SettingsPersonalisationPage from './pageComponents/settings/SettingsPersonalisationPage.js'
 import SettingsBillingPage from './pageComponents/settings/SettingsBillingPage.js'
 import SettingsProfilePage from './pageComponents/settings/SettingsProfilePage.js'
 import ProfilePage from './pageComponents/ProfilePage.js'
-import ChatPage from './pageComponents/chat/ChatPage.js'
-import AddContactPage from './pageComponents/chat/AddContactPage.js'
 import ProjectNoPage from './pageComponents/projects/ProjectNoPage.js'
 import ProjectsPage from './pageComponents/projects/ProjectsPage.js'
 import GeneralBacklog from './pageComponents/GeneralBacklog.js'
@@ -32,14 +29,10 @@ function App() {
 					<Route path="/signUp" exact component={SignUpPage} />
 					<Route path="/paymentPlan" exact component={PaymentPlanPage} />
 					<Route path="/settings/security" exact component={SettingsSecurityPage} />
-					<Route path="/settings/personalisation" exact component={SettingsPersonalisationPage} />
 					<Route path="/settings/billing" exact component={SettingsBillingPage} />
 					<Route path="/settings/profile" exact component={SettingsProfilePage} />
 					<Route path="/profile" exact component={ProfilePage} />
 					<Route path="/notifications" exact component={Notifications} />
-
-					<Route path="/chat" exact component={ChatPage} />
-					<Route path="/chat/addContact" exact component={AddContactPage} />
 					<Route path="/projects" exact component={ProjectsPage} />
 					<Route path="/projects/projectNo" exact component={ProjectNoPage} />
 				</Switch>
